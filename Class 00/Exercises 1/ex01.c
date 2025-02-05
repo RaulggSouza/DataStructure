205a5999c2e4eeda0acd43fcadf12f41afb81b24
RaulggSouza <raulggsouza@hotmail.com>
2025-02-05 19:40:56 -0300

    :sparkles: feat: Completed exercise 01

--- Class 00/Exercises 1/ex01.c
+++ Class 00/Exercises 1/ex01.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
-
+int is_leap_year(int year){
+    if (year < 1582){
+        return -1;
+    }else if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)){
+        return 1;
+    }
+    return 0;
+}
 int main(int argc, char const *argv[]){
-    
+    int n = is_leap_year(2016);
+    printf("%d\n", n);
     return 0;
 }
