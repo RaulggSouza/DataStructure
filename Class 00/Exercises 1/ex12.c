3ca58007bba658d2611f4ff80491e5205039c830
RaulggSouza <raulggsouza@hotmail.com>
2025-02-13 10:44:19 -0300

    Swapping concluded

--- Class 00/Exercises 1/ex12.c
+++ Class 00/Exercises 1/ex12.c
@@ -2,25 +2,24 @@
 
 void troca(int* vet, int start, int end, int pivot){
     int i = start, j = end, aux;
-    if (vet[i] >= vet[pivot]){
-        if (vet[j] <= vet[pivot]){
-            aux = vet[i];
-            vet[i] = vet[j];
-            vet[j] = vet[i];
+    while (j >= i){
+        if (vet[i] >= vet[pivot]){
+            if (vet[j] <= vet[pivot]){
+                aux = vet[i];
+                vet[i] = vet[j];
+                vet[j] = vet[i];
+            }
+            j--;
+        } else {
             i++;
         }
-        j--;
-    } else {
-        i++;
-    }
-    while (j >= i){
-
     }
     
 }
 
-void f1(int* vet, int start, int len){
+void quick(int* vet, int start, int len){
     int pivot = len/2, end = len-1;
+    
     troca(vet, start, end, pivot);
 }
 
@@ -29,7 +28,7 @@ int main(int argc, char const *argv[]){
     for (int i = 0; i < 10; i++){
         printf("%d\t", vector[i]);
     }
-    f1(vector, 0, 10);
+    quick(vector, 0, 10);
     for (int i = 0; i < 10; i++){
         printf("%d\t", vector[i]);
     }
