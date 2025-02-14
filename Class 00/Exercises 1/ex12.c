ef6690c49af11013a9085e803dd916e89d55189a
RaulggSouza <raulggsouza@hotmail.com>
2025-02-14 17:00:23 -0300

    :sparkles: feat: Completed exercise 12

--- Class 00/Exercises 1/ex12.c
+++ Class 00/Exercises 1/ex12.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
 
-void troca(int* vet, int start, int end, int pivot){
+void troca(int* vet, int start, int end, int* pivot){
     int i = start, j = end, aux;
     while (j >= i){
-        if (vet[i] > vet[pivot]){
-            if (vet[j] <= vet[pivot]){
+        if (vet[i] > vet[*pivot]){
+            if (vet[j] <= vet[*pivot]){
                 aux = vet[i];
                 vet[i] = vet[j];
                 vet[j] = aux;
-                if (vet[j] == vet[pivot]) {
-                   pivot = i;
+                if (vet[j] == vet[*pivot]) {
+                    *pivot = i;
                 } 
-                    
             }
             j--;
         } else {
@@ -24,15 +23,17 @@ void quick(int* vet, int start, int len){
     if (len==start){
         return;
     }
-    int pivot = (len+start)/2; //Create a function to choose a better pivot
-    troca(vet, start, len-1, pivot);
+    int pivot = (len+start)/2;
+    troca(vet, start, len-1, &pivot);
+    printf("%d - %d - %d\n", start, len, pivot);
     quick(vet, start, pivot);
     quick(vet, pivot+1, len);
 }
 
 int main(int argc, char const *argv[]){
-     int vector[] = {1,2,3,3,1,2,4,3};
+    //  int vector[] = {1,2,3,3,1,2,4,3};
     // int vector[] = {3,2,2,2,3,4,4,4};
+    int vector[] = {8,7,6,5,4,3,2,1};
     for (int i = 0; i < 8; i++){
         printf("%d\t", vector[i]);
     }
