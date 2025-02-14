03c5e856b61024b39a4d30705527dc2fe195987b
RaulggSouza <raulggsouza@hotmail.com>
2025-02-13 21:33:57 -0300

    Creating Quick Sort base

--- Class 00/Exercises 1/ex12.c
+++ Class 00/Exercises 1/ex12.c
@@ -3,33 +3,42 @@
 void troca(int* vet, int start, int end, int pivot){
     int i = start, j = end, aux;
     while (j >= i){
-        if (vet[i] >= vet[pivot]){
+        if (vet[i] > vet[pivot]){
             if (vet[j] <= vet[pivot]){
                 aux = vet[i];
                 vet[i] = vet[j];
-                vet[j] = vet[i];
+                vet[j] = aux;
+                if (vet[j] == vet[pivot]) {
+                   pivot = i;
+                } 
+                    
             }
             j--;
         } else {
             i++;
         }
     }
-    
 }
 
 void quick(int* vet, int start, int len){
-    int pivot = len/2, end = len-1;
-    
-    troca(vet, start, end, pivot);
+    if (len==start){
+        return;
+    }
+    int pivot = (len+start)/2; //Create a function to choose a better pivot
+    troca(vet, start, len-1, pivot);
+    quick(vet, start, pivot);
+    quick(vet, pivot+1, len);
 }
 
 int main(int argc, char const *argv[]){
-    int vector[] = {1,4,5,2,3,7,4,9,2,1};
-    for (int i = 0; i < 10; i++){
+     int vector[] = {1,2,3,3,1,2,4,3};
+    // int vector[] = {3,2,2,2,3,4,4,4};
+    for (int i = 0; i < 8; i++){
         printf("%d\t", vector[i]);
     }
-    quick(vector, 0, 10);
-    for (int i = 0; i < 10; i++){
+    printf("\n");
+    quick(vector, 0, 8);
+    for (int i = 0; i < 8; i++){
         printf("%d\t", vector[i]);
     }
     return 0;
