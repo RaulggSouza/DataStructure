611596fbc10dded530d95497d06d6f70671fb59e
RaulggSouza <raulggsouza@hotmail.com>
2025-03-25 18:22:35 -0300

    Linked_list activities

--- Class 04 - Linked_List/linked_list.c
+++ Class 04 - Linked_List/linked_list.c
@@ -27,19 +27,45 @@ int size(t_list* list){
     return list->size;
 }
 
-void insert(int elem, int index, t_list* list){
-    t_node* node = (t_node*) malloc(sizeof(t_node));
-    node->item = elem;
-    node->next = NULL;
+t_node* get_item_by_index(t_list* list, int index){
     if (is_empty(list)){
-        list->head = node;
-    } else if (index == 0){
-        t_node* aux = list->head;
-        list->head = node;
-    } else {
-        
+        return NULL;
     }
-    list->size++;
+    t_node* aux = list->head;
+    int i = 0;
+    while (aux != NULL){
+        if (i == index){
+            return aux;
+        }
+        aux = aux->next;
+        i++;
+    }
+    return aux;
+}
+
+t_node* get_node_item(t_list* list, int elem){
+    if (is_empty(list)){
+        return 0;
+    }
+    t_node* aux = list->head;
+    while (aux->item != elem){
+        aux = aux->next;
+    }
+    return aux;
+}
+
+int find(t_list* list, int elem){
+    if (is_empty(list)){
+        return 0;
+    }
+    t_node* aux = list->head;
+    while (aux != NULL){
+        if (aux->item == elem){
+            return 1;
+        }
+        aux = aux->next;
+    }
+    return 0;
 }
 
 void append(int elem, t_list* list){
@@ -55,7 +81,28 @@ void append(int elem, t_list* list){
     list->size++;
 }
 
-int remove_item(t_list* list, t_node* elem){
+void insert(int elem, int index, t_list* list){
+    t_node* node = (t_node*) malloc(sizeof(t_node));
+    node->item = elem;
+    node->next = NULL;
+    if (is_empty(list)){
+        list->head = node;
+    } else if (index == 0){
+        t_node* aux = list->head;
+        list->head = node;
+        node->next = aux;
+    } else if (index >= size(list)){
+        append(elem, list);
+        return;
+    } else {
+       t_node* aux = get_item_by_index(list, index);
+       node->next = aux->next;
+       aux->next = node;
+    }
+    list->size++;
+}
+
+int remove_item(t_list* list, int elem){
     if(is_empty(list)){
         return 0;
     }
@@ -71,26 +118,34 @@ int remove_item(t_list* list, t_node* elem){
     t_node* node = aux->next;
     aux->next = node->next;
     free(node);
+    list->tail = (aux->next == NULL ? aux : aux->next);
+    list->size--;
     return 1;
 }
 
-t_node* get_node_item(t_list* list, int elem){
-    if (is_empty(list)){
-        return 0;
+int remove_at(t_list* list, int index){
+    t_node* node = get_item_by_index(list, index);
+    if (node != NULL){
+        remove_item(list, node->item);
     }
-    t_node* aux = list->head;
-    while (aux->item != elem){
-        aux = aux->next;
-    }
-    return aux;
 }
 
-t_node* get_item_by_index(t_list* list, int index){
-    if (is_empty(list)){
-        return 0;
-    }
-    t_node* aux = list->head;
+t_node* peek_front(t_list* list){
+    return list->head;
+}
 
+t_node* peek_back(t_list* list){
+    return list->tail;
+}
+
+t_node* pop_front(t_list *list){
+    remove_at(list, 0);
+    return peek_front(list);
+}
+
+t_node* pop_back(t_list *list){
+    remove_at(list, list->size--);
+    return peek_back(list);
 }
 
 void print_list(t_list* list){
@@ -99,6 +154,7 @@ void print_list(t_list* list){
         printf("%d ", node->item);
         node = node->next;
     }
+    printf("\n");
 }
 
 void destroy_node(t_list* list, t_node* node){
@@ -114,12 +170,25 @@ void destroy_list(t_list* list){
     free(list);
 }
 
+void clear(t_list* list){
+    destroy_node(list, list->head);
+    list->head = NULL;
+    list->tail = NULL;
+    list->size = 0;
+}
+
 int main(int argc, char const *argv[]){
     t_list* list = create_list();
-    append_tail(3, list); 
-    append_tail(4, list);
-    append_tail(5, list);
-    append_tail(6, list);
+    append(3, list); 
+    append(4, list);
+    append(5, list);
+    append(6, list);
+    insert(23, 0, list);
+    insert(42, 3, list);
+    insert(56, 10, list);
+    print_list(list);
+    clear(list);
+    append(100, list);
     print_list(list);
     return 0;
 }
